6c8ffc5a88e8e5c624df8c418eac2a4d003b71af
99003761 <santosh.kariyavar@ltts.com>
2021-02-26 14:23:45 +0530

    PEP 8 Online checked and Author details added

--- Mini_Project/Mini_Project.py
+++ Mini_Project/Mini_Project.py
@@ -1,3 +1,9 @@
+""" This file Demonstrate the  file operation and OPPs Concepts using Python 3.9.1 version
+Author : Ps No: 99003761 Name: Santosh Kariyavar
+Contact: 8951096674
+Date Of creation: 26/02/2021
+"""
+
 import re
 
 
